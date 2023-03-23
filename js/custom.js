1b02a0f664abbfea0a7f3eeab9b161b0577a48ed
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2023-03-24 04:16:40 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,11 @@ function playVideo() {
   var introVideo = document.getElementById("introVideo");
   introVideo.play();
 }
+
+const handler = document.getElementById("handler");
+if (handler != null) {
+  handler.onclick = function () {
+    uLogo = document.getElementById("u-logo");
+    uLogo.classList.toggle("hide");
+  };
+}
