6c8a53a76042bdcc0aa5ab5dd4121f5b5db6ad13
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2023-03-23 19:23:29 +0600

    intro video play

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function pauseVideo() {
+  var introVideo = document.getElementById("introVideo");
+  introVideo.pause();
+}
+function playVideo() {
+  var introVideo = document.getElementById("introVideo");
+  introVideo.play();
+}
